36144c92c00a1ec271762399de8873292176ecd4
tronerta <tronerta.web@gmail.com>
2019-04-27 12:37:47 +0200

    05_book_title solution

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,20 @@
 class Book
-# write your code here
+	attr_reader :title
+	
+	def title=(new_title)
+		words = new_title.split
+
+		words.each do |word| 
+			index = words.index(word)
+			small_words = ['a', 'an', 'the', 'and', 'in', 'of']
+
+			if index == 0 || words[-1] == word || !small_words.include?(word)
+				word.capitalize!
+			end
+			
+		end
+
+		@title = words.join(' ')
+	end
+
 end
