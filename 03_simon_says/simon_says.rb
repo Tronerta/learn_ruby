fce651991f8bdb3e349d3dd8649274c8c17ef9d2
tronerta <tronerta.web@gmail.com>
2019-04-27 11:42:26 +0200

    03_simon_says solution

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,33 @@
 #write your code here
+def echo input
+	input
+end
+
+def shout input
+	input.upcase!
+end
+
+def repeat input, times = 2
+	((input + ' ') * times)[0...-1]
+end
+
+def start_of_word input, index
+	input[0...index]
+end
+
+def first_word input
+	input.split[0]
+end
+
+def titleize input
+	result = input.split
+
+	result.each do |n| 
+		index = result.index(n)
+		if index == 0 || result[-1] == n || n.length > 4
+			n.capitalize!
+		end 
+	end
+
+	result.join(' ')
+end
