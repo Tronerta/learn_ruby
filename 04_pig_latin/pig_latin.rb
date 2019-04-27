aa8a3dc2b93fca224a83b5907259d373fec5f3c7
tronerta <tronerta.web@gmail.com>
2019-04-27 12:01:07 +0200

    04_pig_latin solution

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,24 @@
 #write your code here
+def translate input
+	vowels = ['a', 'e', 'y', 'u', 'i', 'o']
+	words = input.split
+
+	words.each do |word|
+		chars = word.split('')
+		index = words.index(word)
+
+		while !vowels.include?(chars[0]) do
+			if chars[0..1] == ['q', 'u']
+				first = chars.shift(2)
+				chars << first
+			else
+				first = chars.shift 
+				chars << first
+			end
+		end	
+
+		words[index] = chars.join + 'ay'
+	end
+
+	words.join(' ')
+end
