6e69d9a33f1a09d95a0be860fa0315d841d0bdea
tronerta <tronerta.web@gmail.com>
2019-04-27 11:26:18 +0200

    02_calculator solution

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,24 @@
 #write your code here
+def add a, b
+	a + b
+end
+
+def subtract a, b
+	a - b 
+end
+
+def multiply *rest
+	rest.reduce(:*)
+end
+
+def sum array
+	array.sum
+end
+
+def power a, b
+	a ** b
+end
+
+def factorial a
+	(1..a).reduce(:*)
+end
